6f1141253f925f77784aeec814ebdaede90d626e
John <johngrossi1999@hotmail.com>
2020-07-28 18:10:14 +0100

    added layout, parent which the other pages extend

--- flaskFridge.py
+++ flaskFridge.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-
 def home():
-    return '<h1>Home</h1>'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return '<h1>About</h1>'
+    return render_template('about.html', title = 'about')
 
 #lets me run script through python
 if __name__ == '__main__':
