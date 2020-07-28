b54dfa7fd420bce849ce5931e86bcecd77bfb7d0
John <johngrossi1999@hotmail.com>
2020-07-28 16:58:25 +0100

    flask basics added

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("test")
+import requests
+import json
+
+apiKey = "65a516019eb145fba3d0f251910c1ebc"
+
+myRequest = requests.get("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=65a516019eb145fba3d0f251910c1ebc")
+
+print("no errors")
